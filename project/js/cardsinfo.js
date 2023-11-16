54a674e10161368a78491049ccf2837a296a8242
Constantine-nagornuk <124071530+Constantine-nagornuk@users.noreply.github.com>
2023-11-16 12:53:43 -0500

    9i

--- project/js/cardsinfo.js
+++ project/js/cardsinfo.js
@@ -3,7 +3,7 @@
 const cards = [
 {
 Name: 'Dorritos',
-Size:"Family Size",
+Size:"Large",
 Cost: '10$',
 Picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8GY0239J8-3b3UUV5FwWwydJSCti90Z9izgOhp3JJgjZ-c-UVZNeoKrtYYehroGltrNw&usqp=CAU',
 Type: 'Chips'
@@ -15,9 +15,58 @@ Type: 'Chips'
     Picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8GY0239J8-3b3UUV5FwWwydJSCti90Z9izgOhp3JJgjZ-c-UVZNeoKrtYYehroGltrNw&usqp=CAU',
     Type: 'Chips'
     },
+    {
+        Name: 'Lays Barbecue',
+        Size:"Regular size",
+        Cost: '12$',
+        Picture: 'https://target.scene7.com/is/image/Target/GUEST_d890d5f0-a8eb-4a4a-8634-d5da9baed871?wid=488&hei=488&fmt=pjpeg',
+        Type: 'Chips'
+        
+        },
+        {
+            Name: 'Hershey Chocalte',
+            Size:"Regular size",
+            Cost: '8$',
+            Picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbDXRnGIsbaSAC1sRjmIFgYj0w0J41cMB7BazeiGjO0SbCYspCkQesjD0ubC4GFQAzZ9Q&usqp=CAU',
+            Type: 'Choclate'
+            
+            },
+            {
+                Name: 'Hershey Chocalte-Cookie and cream',
+                Size:"Regular size",
+                Cost: '3$',
+                Picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiaUuEMGwVgTrBQxTSGEJdLg23ok3qB7heqg&usqp=CAU',
+                Type: 'Choclate'
+                
+                },
 ]
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
 const cardHTML = `
         <div class="card">
           <h2 class="Lego#">${}</h2>
@@ -28,4 +77,4 @@ const cardHTML = `
 `
 cards.forEach((card) => {
 
-})
+}) */
